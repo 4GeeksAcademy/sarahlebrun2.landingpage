15bcfb7d6f766a6317a36e4b8035b081a2d4ed90
Sarahlebrun2 <130923257+Sarahlebrun2@users.noreply.github.com>
2023-07-14 14:24:07 +0000

    update card

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer(){
+    return (
+      <div className="footer bg-dark">
+        <p>© 2022 Company, Inc</p>
+      </div>
+    ) 
+}
+export default Footer
+
+
+    
